aaebefb23b2301ef023e3be0107b63d474e62a5e
MSI MODERN <farraszahyf@gmail.com>
2024-05-08 17:10:01 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="container d-flex align-items-center justify-content-between" href="{{ url('/') }}">
+                <a class="container d-flex align-items-center justify-content-between" href="{{route ('dashboard.index')}}">
                 Pasindo Pustaka .</a></h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
